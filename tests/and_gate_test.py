d41b3201ed943e5a716f88a38abad968e156a758
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2026-08-18 12:08:36 +0530

    Added epoches

--- tests/and_gate_test.py
+++ tests/and_gate_test.py
@@ -5,9 +5,27 @@ def step(x):
 
 
 perceptron = Perceptron(2,step)
-perceptron.train([0,0],0)
-perceptron.train([1,1],1)
-perceptron.train([0,1],0)
-perceptron.train([1,0],0)
 
+# train multiple epoches
+no_of_epoches = 10
+
+# epoch
+data = [
+    {"inputs" : [0,0],"output" : 0},
+    {"inputs" : [0,1],"output" : 0},
+    {"inputs" : [1,0],"output" : 0},
+    {"inputs" : [1,1],"output" : 1},
+]
+
+for epoch in range(no_of_epoches):
+    for i in range(4):
+        result = perceptron.train(
+            data[i]["inputs"],
+            data[i]["output"]
+        )
+        print(result)
+        
+print()
+print("Training Complete!!")
+print()
 print(perceptron.predict([1,0]))
